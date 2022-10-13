8e898e58d0e7c1f1a3ae72297d815a2e0f21843e
Zi Xuan <e0958165@u.nus.edu>
2022-10-14 01:26:37 +0800

    First Edit

--- Auth/AuthAPI.py
+++ Auth/AuthAPI.py
@@ -1,6 +1,42 @@
-from flask import Blueprint
+# Score API here
+from flask import Blueprint, request
 import sys
-from db import db
+from flask_cors import cross_origin
+import jwt
+
 sys.path.append("../")
 
 auth_api = Blueprint("auth", __name__)
+
+
+local = {}
+
+
+@auth_api.route("/register", methods=["POST"])
+@cross_origin(supports_credentials=True)
+def register():
+    try:
+        username = request.args.get('username')
+        password = request.args.get('password')
+        local[username] = password
+        return {'status': 'success'}
+    except Exception as e:
+        return {"err": "An error has occured", "status": " failed"}, 500
+    
+
+@auth_api.route("/login", methods=["POST"])
+@cross_origin(supports_credentials=True)
+def login():
+    try:
+        username = request.args.get('username')
+        password_hash = request.args.get('passwordHash')
+        if local.get(username) == None:
+            return {"status": "failed", "err": "username and password does not match"}
+        elif local[username] != password_hash:
+            return {"status": "failed", "err": "username and password does not match"}
+        else:
+            key = 'secret'
+            encoded = jwt.encode(local, key)
+            return {"status": "success", "data": encoded}
+    except Exception as e:
+        return {"err": "An error has occured", "status": " failed"}, 500
--- main.py
+++ main.py
@@ -10,3 +10,12 @@ app = Flask(__name__)
 
 app.register_blueprint(profiles_api, url_prefix="/profiles")
 app.register_blueprint(auth_api, url_prefix="/auth")
+
+@app.route("/", methods = ["GET"])
+def hello():
+    return "Welcome!"
+
+
+if __name__ == "__main__":
+    #app.run()
+    app.run("localhost",port=8080)
